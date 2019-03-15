c8fdc93bf5666c1d40f9c97b65c47a9fc910e06e
Jan de Jongh <jfcmdejongh@gmail.com>
2019-03-16 00:09:24 +0100

    Progress on LICENSE, comments and javadoc.

--- src/main/java/org/javajdj/jservice/midi/device/rolandboss/ParameterDescriptor_RolandBoss.java
+++ src/main/java/org/javajdj/jservice/midi/device/rolandboss/ParameterDescriptor_RolandBoss.java
@@ -1,13 +1,31 @@
+/* 
+ * Copyright 2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package org.javajdj.jservice.midi.device.rolandboss;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.javajdj.jservice.midi.device.MidiDevice;
 import org.javajdj.jservice.midi.device.ParameterDescriptor;
 import org.javajdj.util.hex.HexUtils;
 
-/**
+/** Description of a parameter (key, value pair) in a {@link MidiDevice},
+ *  in particular for Roland-Boss devices.
  *
  * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
  * 
@@ -16,7 +34,19 @@ public class ParameterDescriptor_RolandBoss<E>
   implements ParameterDescriptor
 {
   
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
   private static final Logger LOG = Logger.getLogger (ParameterDescriptor_RolandBoss.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTORS / FACTORIES / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   protected ParameterDescriptor_RolandBoss
   ( final ParameterType_RolandBoss parameterType_RolandBoss,
