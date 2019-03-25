79654a8f860cca782e82d27bfbfe74e0da4273cd
Jan de Jongh <jfcmdejongh@gmail.com>
2019-03-25 20:03:56 +0100

    Imports.

--- src/main/java/org/javajdj/jservice/midi/device/rolandboss/bossme80/swing/JMe80Panel_Device.java
+++ src/main/java/org/javajdj/jservice/midi/device/rolandboss/bossme80/swing/JMe80Panel_Device.java
@@ -20,7 +20,6 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
