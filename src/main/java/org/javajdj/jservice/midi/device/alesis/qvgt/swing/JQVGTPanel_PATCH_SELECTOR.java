5f4c3e2adbc1b8fef0cc227e97d5eab18b75d1ca
Jan de Jongh <jfcmdejongh@gmail.com>
2019-04-30 10:45:23 +0200

    A {@link JPanel} for selecting the current patch on an Alesis Quadraverb GT.

---
+++ src/main/java/org/javajdj/jservice/midi/device/alesis/qvgt/swing/JQVGTPanel_PATCH_SELECTOR.java
@@ -0,0 +1,75 @@
+/* 
+ * Copyright 2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jservice.midi.device.alesis.qvgt.swing;
+
+import java.awt.GridLayout;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import org.javajdj.jservice.midi.device.MidiDevice;
+import org.javajdj.jservice.midi.device.alesis.qvgt.MidiDevice_QVGT;
+import org.javajdj.jservice.midi.device.swing.JMidiDeviceParameter;
+import org.javajdj.jservice.midi.device.swing.JMidiDeviceParameter_Integer_Slider;
+
+/**A {@link JPanel} for selecting the current patch on an Alesis Quadraverb GT.
+ * 
+ * <p>
+ * Subordinate to {@link JQVGTPanel}.
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+final class JQVGTPanel_PATCH_SELECTOR
+  extends JPanel
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JQVGTPanel_PATCH_SELECTOR.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTORS / FACTORIES / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JQVGTPanel_PATCH_SELECTOR (final MidiDevice midiDevice)
+  {
+    super ();
+    setLayout (new GridLayout (6, 1, 5, 5));
+    add (new JLabel ());
+    add (new JLabel ());
+    final JMidiDeviceParameter jPatchSelector = new JMidiDeviceParameter_Integer_Slider (midiDevice,
+      "Current Program", MidiDevice_QVGT.CURRENT_PATCH_NO_NAME, 0, 99);
+    jPatchSelector.setDisableWhenNull (false);
+    add (jPatchSelector);
+    add (new JLabel ());
+    add (new JLabel ());
+    add (new JLabel ());
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
