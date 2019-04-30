a343bd16fc4a06b456ed16a6770ab554aa50b206
Jan de Jongh <jfcmdejongh@gmail.com>
2019-04-30 10:53:50 +0200

    A {@link JPanel} for monitoring and editing MODULATION parameters of an Alesis Quadraverb GT {@link MidiDevice}.
    Placeholder; MODULATION is not yet implemented.

---
+++ src/main/java/org/javajdj/jservice/midi/device/alesis/qvgt/swing/JQVGTPanel_MODULATION.java
@@ -0,0 +1,162 @@
+/* 
+ * Copyright 2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jservice.midi.device.alesis.qvgt.swing;
+
+import java.awt.GridLayout;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import org.javajdj.jservice.midi.device.MidiDevice;
+import org.javajdj.jservice.midi.device.MidiDeviceListener;
+import org.javajdj.jservice.midi.device.alesis.qvgt.MidiDevice_QVGT;
+import org.javajdj.jservice.midi.device.alesis.qvgt.Patch_QGVT;
+import org.javajdj.jservice.midi.device.swing.JMidiDeviceParameter;
+import org.javajdj.swing.SwingUtilsJdJ;
+
+/** A {@link JPanel} for monitoring and editing MODULATION parameters of an Alesis Quadraverb GT {@link MidiDevice}.
+ * 
+ * <p>
+ * Auxiliary file to {@link JQVGTPanel}.
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ * @see MidiDevice
+ * @see MidiDevice_QVGT
+ * 
+ */
+final class JQVGTPanel_MODULATION extends JPanel
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTORS / FACTORIES / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Constructs the panel.
+   * 
+   * @param midiDevice The MIDI device, must be non-{@code null} and, at the present time, a {@link MidiDevice_QVGT}.
+   * 
+   * @throws IllegalArgumentException If the MIDI device is {@code null} or of illegal type.
+   * 
+   */
+  public JQVGTPanel_MODULATION (final MidiDevice midiDevice)
+  {
+    super ();
+    if (midiDevice == null || ! (midiDevice instanceof MidiDevice_QVGT))
+      throw new IllegalArgumentException ();
+    this.midiDevice = midiDevice;
+    setLayout (new GridLayout (9, 1, 5, 0));    
+//    addMidiDeviceParameter (new JMidiDeviceParameter_Enum (midiDevice,
+//      "Mode", MidiDevice_QVGT.EDIT_BUFFER_DELAY_MODE_NAME, MidiDevice_QVGT.DelayMode.class));
+    setGuiParameters ((Patch_QGVT.Configuration) midiDevice.get (MidiDevice_QVGT.EDIT_BUFFER_CONFIG_NAME));
+    midiDevice.addMidiDeviceListener (this.midiDeviceListener);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // MIDI DEVICE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final MidiDevice midiDevice;
+  
+  private MidiDevice getMidiDevice ()
+  {
+    return this.midiDevice;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // MIDI DEVICE LISTENER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final MidiDeviceListener midiDeviceListener = (final Map<String, Object> changes) ->
+  {
+    if (changes == null)
+      return;
+    if (changes.containsKey (MidiDevice_QVGT.EDIT_BUFFER_CONFIG_NAME))
+    {
+      SwingUtilsJdJ.invokeOnSwingEDT (
+      // XXX The commented-out version should work as well; apparently our device does not provide the new values properly [?].
+      // () -> setGuiParameters ((Patch_QGVT.Configuration) changes.get (MidiDevice_QVGT.EDIT_BUFFER_CONFIG_NAME)));
+      () -> setGuiParameters ((Patch_QGVT.Configuration) getMidiDevice ().get (MidiDevice_QVGT.EDIT_BUFFER_CONFIG_NAME)));
+    }
+  };
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // PARAMETER MAP
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final Map<String, JMidiDeviceParameter> parameterMap = new HashMap<> ();
+
+  private void addMidiDeviceParameter (final JMidiDeviceParameter midiDeviceParameter)
+  {
+    if (midiDeviceParameter == null || this.parameterMap.containsKey (midiDeviceParameter.getKey ()))
+      throw new IllegalArgumentException ();
+    this.parameterMap.put (midiDeviceParameter.getKey (), midiDeviceParameter);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SET GUI PARAMETERS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private void setGuiParameters (final Patch_QGVT.Configuration configuration)
+  {
+    removeAll ();
+    if (configuration != null)
+    {
+      switch (configuration)
+      {
+        case C1_EQ_PCH_DL_REV:
+        case C2_LES_DL_REV:
+        case C3_GEQ_DL:
+        case C4_5EQ_PCH_DL:
+        case C5_3EQ_REV:
+        case C6_RING_DL_REV:
+        case C7_RESO_DL_REV:
+        case C8_SAMPLING:
+        {
+          setLayout (new GridLayout (1, 1, 5, 0));
+          final JLabel label = new JLabel ("Not Implemented");
+          label.setHorizontalAlignment (SwingConstants.CENTER);
+          add (label);
+          break;
+        }
+        default:
+          throw new RuntimeException ();
+      }
+    }
+    // We need to validate (); but this was found through trial-and-error.
+    validate ();
+    repaint ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
